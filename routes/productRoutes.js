dc987758b3e2a49267b2961b9960cf28a65a8322
ShoxjahonNiyatov456 <karolprosta11@gmail.com>
2025-09-19 15:27:36 +0500

    reorder added

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,32 +1,33 @@
 import express from "express";
 import {
-    createProduct,
-    getProducts,
-    getProductById,
-    updateProduct,
-    deleteProduct,
+  createProduct,
+  getProducts,
+  getProductById,
+  updateProduct,
+  deleteProduct,
+  reorderProducts,
 } from "../controllers/productController.js";
 import upload from "../middlewares/uploadMiddleware.js";
 import { protect, adminOnly } from "../middlewares/auth.js";
 
 const router = express.Router();
 
+// Barcha productlar
 router.get("/", getProducts);
+
+// Bitta product
 router.get("/:id", getProductById);
-router.post(
-    "/",
-    protect,
-    adminOnly,
-    upload.fields([{ name: "images", maxCount: 5 }]),
-    createProduct
-);
-router.put(
-    "/:id",
-    protect,
-    adminOnly,
-    upload.fields([{ name: "images", maxCount: 5 }]),
-    updateProduct
-);
+
+// Yangi product qo'shish
+router.post("/", protect, adminOnly, upload.array("images"), createProduct);
+
+// Productni yangilash
+router.put("/:id", protect, adminOnly, upload.array("images"), updateProduct);
+
+// Productni o‘chirish
 router.delete("/:id", protect, adminOnly, deleteProduct);
+
+// Reorder products
+router.put("/reorder/all", protect, adminOnly, reorderProducts);
 
 export default router;
